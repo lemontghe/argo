8911a19eb2db927e2d998c86e959299ed46dafc5
lemontghe <ha5744027@gmail.com>
2021-02-08 15:30:58 +0330

    .

--- argo/settings.py
+++ argo/settings.py
@@ -37,7 +37,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.runserver_nostatic',
+    #  'whitenoise.runserver_nostatic',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
