32327eb87837cd22088b17eb96987685dfeb097b
lemontghe <ha5744027@gmail.com>
2021-02-08 08:56:17 +0330

    using git for heroku deployment

--- argo/settings.py
+++ argo/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from os import path, environ
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -76,6 +77,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 #  DATABASES = {
     #  'default': {
