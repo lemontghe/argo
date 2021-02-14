8f52f95e04972e753fc2cdbddcf5705e5a17ab0f
lemontghe <ha5744027@gmail.com>
2021-02-14 18:31:29 +0330

    .

--- argo/settings.py
+++ argo/settings.py
@@ -43,19 +43,34 @@ MIDDLEWARE = [
 ]
 ROOT_URLCONF = 'argo.urls'
 SITE_ID=1,
+#  TEMPLATES = [
+    #  {
+        #  'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        #  'DIRS': [],
+        #  'APP_DIRS': True,
+        #  'OPTIONS': {
+            #  'context_processors': [
+                #  'django.template.context_processors.debug',
+                #  'django.template.context_processors.request'
+                #  'django.contrib.auth.context_processors.auth',
+                #  'django.contrib.messages.context_processors.messages',
+            #  ],
+        #  },
+    #  },
+#  ]
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        #  'DIRS': [],
-        'DIRS': [path.join(BASE_DIR, 'templates')],
+        'DIRS': [path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request'
-                #  'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+
         },
     },
 ]
