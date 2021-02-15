f339e3ec2a8ced16ffba93152bf70ff7ba751957
lemontghe <ha5744027@gmail.com>
2021-02-15 11:02:13 +0330

    .

--- frontend/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Ad, AdsPlan, PlansPlan
-
-
-admin.site.register(Profile)
-admin.site.register(Ad)
-admin.site.register(AdsPlan)
-admin.site.register(PlansPlan)
--- frontend/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class FrontendConfig(AppConfig):
-    name = 'frontend'
-
-    def ready(self):
-        import frontend.signals
