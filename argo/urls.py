26fc4dfa361e7392c90b4ff0727a806673b89a2e
lemontghe <ha5744027@gmail.com>
2021-02-15 15:47:30 +0330

    .

--- argo/urls.py
+++ argo/urls.py
@@ -22,8 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #  path('app/', include('django.contrib.auth.urls')),
-    #  path('api/', include('api.urls')),
     path('', include('frontend.urls'))
 
 ]
