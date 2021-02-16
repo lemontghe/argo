10d162e9394117d064cb3ba229f02fe7632fac52
lemontghe <ha5744027@gmail.com>
2021-02-16 12:35:45 +0330

    .

--- frontend/utils.py
+++ frontend/utils.py
@@ -1,9 +1,14 @@
 from time import sleep
 
 
-def generate_ref_code():
+def generate_ref_code(i):
     with open("frontend/user_id", 'r') as f:
         a = int(f.readline())
-    with open("frontend/user_id", 'w') as f:
-        f.write(str(a+1))
-    return a+1
+    if i:
+        with open("frontend/user_id", 'w') as f:
+            f.write(str(a+1))
+        return a+1
+    else:
+        with open("frontend/user_id", 'w') as f:
+            f.write(str(a-1))
+        return a-1
