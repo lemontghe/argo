067a3f01d0a1331da3dbc45f64f794c1de07c402
lemontghe <ha5744027@gmail.com>
2021-02-18 20:01:04 +0330

    .

--- frontend/views.py
+++ frontend/views.py
@@ -19,9 +19,11 @@ from django.views.decorators.http import require_GET
 with open("frontend/user_id", 'r') as f:
     try:
         a = int(f.readline())
-    except: a = 9999
+    except: pass
+    if str(f.readline()) == '': a = 9999
 with open("frontend/user_id", 'w') as f:
     if a != Profile.objects.all().count()+9999:
+        print(Profile.objects.all().count()+9999)
         f.write(str(Profile.objects.all().count()+9999))
 
 class PasswordResetingView(PasswordResetView):
