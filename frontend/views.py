ddd20c2ebcc76d7b54ac0a8333825962929136f0
lemontghe <ha5744027@gmail.com>
2021-03-01 07:07:59 +0330

    .

--- frontend/views.py
+++ frontend/views.py
@@ -308,6 +308,8 @@ def payment(request, *args, **kwargs):
 def plans(request, *args, **kwargs):
     user_obj = User.objects.get(username=str(request.user.username))
     profile = Profile.objects.get(user=user_obj)
+    if profile is None:
+        return render(request, 'frontend/plans.html')
     if profile.per_hour in ['per_hour', None]: profile.per_hour = 0
 
     a = pytz.utc.localize(datetime.utcnow())
